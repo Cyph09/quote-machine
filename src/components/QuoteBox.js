a730cf1f4ee7e0deed3f1c3f9ea3d311751165aa
Swaleh Mhina <cyph09@gmail.com>
2019-11-28 19:43:25 +0300

    Initial commit

---
+++ src/components/QuoteBox.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+class QuoteBox extends Component {
+  render() {
+    return (
+      <div id="quote-box">
+        <h1>Random quote machine</h1>
+      </div>
+    );
+  }
+}
+export default QuoteBox;
