8b020fa32b46fe8c244c6ccaa29813e31c97fb4d
Swaleh Mhina <cyph09@gmail.com>
2019-11-30 22:27:41 +0300

    Updates

---
+++ precache-manifest.08f41e1d2abb63d892647761a96cbe84.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10486f36e88705c3ae413167ed5436ee",
+    "url": "/quote-machine/index.html"
+  },
+  {
+    "revision": "1f159c15aa92a7cea567",
+    "url": "/quote-machine/static/css/main.31dcc73e.chunk.css"
+  },
+  {
+    "revision": "f8869ac0601a0e9836d0",
+    "url": "/quote-machine/static/js/2.c294cce5.chunk.js"
+  },
+  {
+    "revision": "1f159c15aa92a7cea567",
+    "url": "/quote-machine/static/js/main.28df8e1f.chunk.js"
+  },
+  {
+    "revision": "4a4889e2db76834b8ca4",
+    "url": "/quote-machine/static/js/runtime-main.6cec46d2.js"
+  }
+]);
